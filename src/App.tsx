2554e82238528c622915edb9c4b662f0f5ed88b4
hilalozturk1 <ozhilal222@gmail.com>
2025-02-10 00:43:43 +0300

    tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import './index.css';
 
-function App() {
+import React from "react";
+import FlightsSearch from './pages/FlightsSearch';
+
+const App: React.FC = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div className="bg-gray-100 min-h-screen p-4">
+      <h1 className="text-2xl font-bold text-center mb-4">Google Flights Clone</h1>
+      <FlightsSearch />
+    </div>
+  );
+};
 
-export default App
+export default App;
+
