0cab0dc28155de77d85756c13f800c799c723dba
hilalozturk1 <ozhilal222@gmail.com>
2025-02-10 01:15:25 +0300

    rapid api configuration

---
+++ src/config/apiConfig.ts
@@ -0,0 +1,6 @@
+export const API_URL = import.meta.env.VITE_API_URL as string;
+
+export const API_HEADERS = {
+  "X-RapidAPI-Key": import.meta.env.VITE_RAPIDAPI_KEY as string,
+  "X-RapidAPI-Host": import.meta.env.VITE_RAPIDAPI_HOST as string,
+};
